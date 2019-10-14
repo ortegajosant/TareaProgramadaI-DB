55b22544bd36d6cd25eb5de1c883d2f6d1fda619
Esteban <estebandcg1999@gmail.com>
2019-10-13 23:19:07 -0600

    Se añade la funcion para realizar el reporte del abastecimiento a las sucursales

---
+++ PostgreSQL/Scripts/abastecimiento.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION ReporteAbastecimiento()
+RETURNS VOID AS $$
+BEGIN
+	COPY (
+		SELECT S.IdSucursal, P.IdProducto, COUNT(A.IdArticulo) AS Cantidad
+		FROM Sucursal AS S
+		INNER JOIN Articulo AS A ON A.IdSucursal = S.idsucursal
+		INNER JOIN Producto AS P ON P.IdProducto = A.IdProducto
+		GROUP BY S.IdSucursal, P.IdProducto
+	)
+	To 'C:\Users\Public\test.csv' DELIMITER ';' CSV HEADER;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION ReporteAbastecimiento();
+SELECT ReporteAbastecimiento();
+
+--'C:\Users\este0\Desktop\Esteban\TEC\2019 - II Semestre\Bases de Datos\Sk84TEC\test.csv'
+
+--SELECT S.IdSucursal, P.IdProducto, COUNT(A.IdArticulo) AS Cantidad
+--FROM Sucursal AS S
+--INNER JOIN Articulo AS A ON A.IdSucursal = S.idsucursal
+--INNER JOIN Producto AS P ON P.IdProducto = A.IdProducto
+--GROUP BY S.IdSucursal, P.IdProducto;
+
+
